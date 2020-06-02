0542123f7e5ac5c956eb71a4a4b6479256e0ce3d
sujalgera01 <sujalgera01@gmail.com>
2020-06-02 14:58:41 +0530

    changes

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
 
 </head>
 <body>
-	<h1>Online Service Provider <span><i class="material-icons">person</i></span></h1>
-	<h4>Welcome to OSP</h4>
+	<h1>MY SERVICES<span><i class="material-icons">person</i></span></h1>
+	<h4>Welcome to My Services</h4>
 	<p class="para1">This is a simple WEB-APP that will make your life easier. Here you can give us the order to bring the products to you from anywhere and at any time. Just give us the following details and we will be there to help you out !!</p>
 	<h3> <span class="s1"> SAVE YOUR TIME. SAVE YOUR FUEL <br> </span><span class="s2">and</span><br><span class="s3"> click the below button to give us the Order. </span></h3>
 	<form action="form.php" method="post">
 	<button type="order" name="order" class="btn">GIVE ORDER</button>
 	</form>
+	 <small>&copy; Sujal Gera</small>
 </body>
 </html>
